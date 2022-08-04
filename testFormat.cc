dc5bc2744b5168da2b0b339fda739b97e0789145
huanghe <glibcxxhuang@outlook.com>
2022-08-04 09:23:41 +0200

    Enter

--- testFormat.cc
+++ testFormat.cc
@@ -1,6 +1,15 @@
 #include <fmt/core.h>
+#include <string>
+#include <filesystem>
+
+namespace fs = std::filesystem;
 
 int main() 
-{
-    fmt::print("Hello, world!\n");
+{   
+    fs::path pTs("/home/huanghe/GithubRepo/MinecraftServerBackup");
+    std::string s = pTs.filename();
+    fmt::print("Current path is{1}\n",s);
+
+    return 0;
 }
+//
